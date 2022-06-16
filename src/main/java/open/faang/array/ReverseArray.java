511778905461b67370697c8c8d82d881d07edaee
imsanzay <imsanzaypatel@gmail.com>
2022-06-16 19:40:22 +0530

    ReverseArray Using BrutForce

--- src/main/java/open/faang/array/ReverseArray.java
+++
@@ -1,4 +0,0 @@
-package open.faang.array;
-
-public class ReverseArray {
-}
---
+++ src/main/java/open/faang/array/ReverseArray1.java
@@ -0,0 +1,15 @@
+package open.faang.array;
+
+public class ReverseArray1 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7};
+        reverse(arr);
+    }
+
+    // array reverse using brut force
+    private static void reverse(int[] arr) {
+    for(int i = arr.length-1;i>0;i--){
+        System.out.print(arr[i]+",");
+    }
+    }
+}
